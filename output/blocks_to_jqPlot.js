0ec9983e8a9033076e9b73f53c141ee16d0db5de
CarbonMan <CarbonMan@users.noreply.github.com>
2017-05-31 09:33:58 +1000

    Update blocks_to_jqPlot.js

--- output/blocks_to_jqPlot.js
+++ output/blocks_to_jqPlot.js
@@ -2,7 +2,7 @@
 * Generate an jqPlot chart statement
 */
 T$.on("load toolbox blocks", function(ev){
-    var cat = $("category[name='" + T$.i18n['Display'] + "']");
+    var cat = $("category[name='" + T$.i18n('Display') + "']");
     if (cat){
         $('<block type="line_chart"></block>').appendTo(cat);
         $('<block type="series_label"></block>').appendTo(cat);
@@ -15,22 +15,22 @@ T$.on("load toolbox blocks", function(ev){
 Blockly.Blocks['line_chart'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField(T$.i18n["Line chart"]);
+            .appendField(T$.i18n("Line chart"));
         this.appendDummyInput()
-            .appendField(T$.i18n["Chart title"])
+            .appendField(T$.i18n("Chart title"))
             .appendField(new Blockly.FieldTextInput("               "), "title");
         this.appendDummyInput()
-            .appendField(T$.i18n["Y axis title"])
+            .appendField(T$.i18n("Y axis title"))
             .appendField(new Blockly.FieldTextInput("               "), "yAxis");
         this.appendDummyInput()
-            .appendField(T$.i18n["X axis title"])
+            .appendField(T$.i18n("X axis title"))
             .appendField(new Blockly.FieldTextInput("               "), "xAxis");
         this.appendDummyInput()
-            .appendField(T$.i18n["First column is X axis ticks"])
+            .appendField(T$.i18n("First column is X axis ticks"))
             .appendField(new Blockly.FieldCheckbox("FALSE"), "firstColumnXaxisTicks");
         this.appendStatementInput("labels")
             .setCheck("series_label")
-            .appendField(T$.i18n["Series labels"]);
+            .appendField(T$.i18n("Series labels"));
         this.setPreviousStatement(true, "display");
         //this.setNextStatement(true, "statement");
         this.setColour(230);
@@ -42,7 +42,7 @@ Blockly.Blocks['line_chart'] = {
 Blockly.Blocks['series_label'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField(T$.i18n["Label"])
+            .appendField(T$.i18n("Label"))
             .appendField(new Blockly.FieldTextInput("series_name"), "name");
         this.setPreviousStatement(true, "series_label");
         this.setNextStatement(true, "series_label");
