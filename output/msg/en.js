b7dffe3378c31a6018d4430026015266c5f11f1f
CarbonMan <CarbonMan@users.noreply.github.com>
2017-05-30 15:31:13 +1000

    Create en.js

---
+++ output/msg/en.js
@@ -0,0 +1,10 @@
+T$.i18n = $.extend(T$.i18n, {
+  'Display': 'Display',
+  "Line chart": "Line chart",
+  "Chart title": "Chart title",
+  "Y axis title": "Y axis title",
+  "X axis title": "X axis title",
+  "First column is X axis ticks": "First column is X axis ticks",
+  "Series labels": "Series labels",
+  "Label": "Label"
+});
