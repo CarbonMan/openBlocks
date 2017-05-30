5505233cd299ce8e07a93a70a38d326ac500ecff
CarbonMan <CarbonMan@users.noreply.github.com>
2017-05-30 15:31:28 +1000

    Update codeBlocks.js

--- code/codeBlocks.js
+++ code/codeBlocks.js
@@ -1,5 +1,5 @@
-IOTKEY.on("load toolbox blocks", function(ev){
-    var cat = $("category[name='" + IOTKEY.i18n['Display'] + "']");
+T$.on("load toolbox blocks", function(ev){
+    var cat = $("category[name='" + T$.i18n['Display'] + "']");
     if (cat){
         $('<block type="line_chart"></block>').appendTo(cat);
         $('<block type="series_label"></block>').appendTo(cat);
