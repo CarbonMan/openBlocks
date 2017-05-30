565b833f9975d66b7346e98d246d0fcaed4420c8
CarbonMan <CarbonMan@users.noreply.github.com>
2017-05-31 07:28:58 +1000

    Delete codeBlocks.js

--- code/codeBlocks.js
+++
@@ -1,7 +0,0 @@
-T$.on("load toolbox blocks", function(ev){
-    var cat = $("category[name='" + T$.i18n['Display'] + "']");
-    if (cat){
-        $('<block type="line_chart"></block>').appendTo(cat);
-        $('<block type="series_label"></block>').appendTo(cat);
-    }
-});
