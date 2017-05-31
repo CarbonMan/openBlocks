9fb7cb68bb853adc8dd59e1fb282184bd3dc86b3
CarbonMan <CarbonMan@users.noreply.github.com>
2017-05-31 22:32:39 +1000

    Update blocks_to_jqPlot.js

--- output/dataPlot/blocks_to_jqPlot.js
+++ output/dataPlot/blocks_to_jqPlot.js
@@ -3,7 +3,7 @@
 */
 T$.on("load toolbox blocks", function(ev){
     var cat = $("category[name='" + T$.i18n('Display') + "']");
-    if (cat){
+    if (cat.length){
         $('<block type="line_chart"></block>').appendTo(cat);
         $('<block type="series_label"></block>').appendTo(cat);
     }
