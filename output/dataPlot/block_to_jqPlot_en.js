e1773c71daf6e7a145ffd27f8eabc10cea737865
CarbonMan <CarbonMan@users.noreply.github.com>
2017-05-31 22:10:07 +1000

    Create block_to_jqPlot_en.js

---
+++ output/dataPlot/block_to_jqPlot_en.js
@@ -0,0 +1,13 @@
+/**
+* English message file for block_to_jqPlot.js
+*/
+T$.L10n = $.extend(T$.L10n, {
+  'Display': 'Display',
+  "Line chart": "Line chart",
+  "Chart title": "Chart title",
+  "Y axis title": "Y axis title",
+  "X axis title": "X axis title",
+  "First column is X axis ticks": "First column is X axis ticks",
+  "Series labels": "Series labels",
+  "Label": "Label"
+});
