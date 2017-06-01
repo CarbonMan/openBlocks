33f925f80a668f931a6317af58c8c520e8469718
CarbonMan <CarbonMan@users.noreply.github.com>
2017-06-02 08:58:02 +1000

    Create baseBlock_Javascript.js

---
+++ codeBlocks/baseBlock_Javascript.js
@@ -0,0 +1,17 @@
+Blockly.JavaScript['codeBlock'] = function (block) {
+    var code = Blockly.JavaScript.statementToCode(block, 'contained_code');
+    var ev = {
+        context: block.T$.context,
+        outputType: "string",
+        type: "javascript",
+        code: code
+    };
+    // Allow blocks (such as nlsql_verbal_output to restructure the code)
+    block.IOTKEY.fire("generate", ev)
+    console.log(ev.code);
+    // The type can be set to hub/nlSql meaning it is to be executed on the hub
+    return "<codeBlock type='" + ev.type + "' id='" + escape(block.id) + "'" +
+        " outputType='" + ev.outputType + "'>" +
+        escape(ev.code) +
+        "</codeBlock>";
+};
