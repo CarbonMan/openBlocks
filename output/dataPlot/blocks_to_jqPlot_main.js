18a0b3beba51179211c2617d7268066b6173bd39
CarbonMan <CarbonMan@users.noreply.github.com>
2017-06-11 12:29:13 +1000

    Create blocks_to_jqPlot_main.js

--- output/dataPlot/blocks_to_jqPlot_main.js
+++ output/dataPlot/blocks_to_jqPlot_main.js
@@ -1,6 +1,6 @@
 $(function(){
   // Build the toolbox categories and load the blocks into them.
-  // The blocks have a custom function T$_toolbox to make it easier to 
+  // The blocks have a custom function toolbox$ to make it easier to 
   // centralize the code.
   T$.on("load toolbox categories", function(ev){
     var cat = ev.toolbox.find("category[name='" + T$.i18n('Display') + "']");
