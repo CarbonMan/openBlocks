5d8388c5f0fce60bf806d809ea77bf99770c5023
CarbonMan <CarbonMan@users.noreply.github.com>
2017-06-13 21:56:28 +1000

    Create baseBlock.js

--- codeBlocks/baseBlock.js
+++ codeBlocks/baseBlock.js
@@ -12,30 +12,9 @@ T$.on("load toolbox categories", function(ev){
   */
 });
 
-Blockly.Blocks['task'] = {
-    init: function () {
-        this.appendStatementInput("script")
-            .setCheck(['statement', 'display'])
-            .appendField(T$.i18n("Task"));
-        this.setPreviousStatement(true, "task");
-        this.setNextStatement(true, "task");
-        this.setColour(230);
-        this.setTooltip('');
-        this.setHelpUrl('');
-        /*
-        // Used while generating code (see nlSqlBlockGenerator.js)
-        eventHandler(this);
-        // Called by statements during generation
-        this.T$ = {
-            context: {}
-        };
-        eventHandler(this.T$);
-        */
-    },
-    toolbox$: function ($toolbox) {
-      var cat = $toolbox.find("category[name='" + T$.i18n('Task') + "']");
-      if (cat.length) 
-        $('<block type="task"></block>').appendTo(cat);
-    }
+Blockly.Blocks['task'].toolbox$ = function ($toolbox) {
+    var cat = $toolbox.find("category[name='" + T$.i18n('Task') + "']");
+    if (cat.length) 
+      $('<block type="task"></block>').appendTo(cat);
 };
 
