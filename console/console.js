c581ecab9c907f59b4b4eb4dd5ca9680d7d40e5a
CarbonMan <CarbonMan@users.noreply.github.com>
2017-06-13 12:05:04 +1000

    Initial

---
+++ console/console.js
@@ -0,0 +1,42 @@
+$(function () {
+  // Build the debug category and load the block into it.
+  // The blocks have a custom function toolbox$ to make it easier to 
+  // centralize the code.
+  T$.on("load toolbox categories", function(ev){
+    // Called once all openBlocks have been downloaded (see blocklyCode.js)
+    // Don't call the buildCustomBlocks until ready otherwise the blocks won't be ready.
+    T$.nlSql.blocks.buildCustomBlocks();
+    var dbgCat = ev.toolbox.find("category[name='" + T$.i18n('Debug') + "']");
+    if (!dbgCat.length) 
+        dbgCat = $("<category name='" + T$.i18n('Debug') + "'></category>").appendTo(ev.toolbox);
+        // Console logging
+    var newCat = dbgCat.find("category[name='" + T$.i18n('Console') + "']");
+    if (!newCat.length) 
+        newCat = $("<category name='" + T$.i18n('Console') + "'></category>").appendTo(qryCat);
+
+  });  
+});
+
+Blockly.Blocks['console'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Display")
+        .appendField(new Blockly.FieldDropdown([["verbose","verbose"], ["info","info"], ["warn","warning"], ["error","error"]]), "NAME");
+    this.appendValueInput("NAME")
+        .setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  },
+		toolbox$: function ($toolbox) {
+			var cat = $toolbox.find("category[name='" + T$.i18n('Debug') + "']");
+			if (cat) {
+				$('<block type="console">' +
+					'<field name="tableName">name...</field>' +
+					'</block>').appendTo(cat);
+			}
+		}
+};
