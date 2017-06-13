a007c997a732344ffc445761424e579d2ca9a808
CarbonMan <CarbonMan@users.noreply.github.com>
2017-06-13 12:15:40 +1000

    Create console_Javascript.js

---
+++ console/console_Javascript.js
@@ -0,0 +1,7 @@
+Blockly.JavaScript['console'] = function(block) {
+  var type = block.getFieldValue('type');
+  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'console." + type + "(" + value_name + ");\n';
+  return code;
+};
