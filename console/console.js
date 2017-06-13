e024b43ffe0c22b597af4bece5750e16edf12a47
CarbonMan <CarbonMan@users.noreply.github.com>
2017-06-13 21:52:14 +1000

    Create console.js

--- console/console.js
+++ console/console.js
@@ -15,29 +15,10 @@ $(function () {
   });  
 });
 
-Blockly.Blocks['console'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Display")
-        .appendField(new Blockly.FieldDropdown([
-	    [ T$.i18n("verbose"),"verbose"], 
-	    [ T$.i18n("info"),"info"], 
-	    [ T$.i18n("warn"),"warning"], 
-	    [ T$.i18n("error"),"error"]]), "type");
-    this.appendValueInput("NAME")
-        .setCheck(null);
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
-    this.setTooltip('');
-    this.setHelpUrl('');
-  },
-	toolbox$: function ($toolbox) {
-		var cat = $toolbox.find("category[name='" + T$.i18n('Console') + "']");
-		if (cat) {
-			$('<block type="console">' +
-				'</block>').appendTo(cat);
-		}
+Blockly.Blocks['console'].toolbox$ = function ($toolbox) {
+	var cat = $toolbox.find("category[name='" + T$.i18n('Console') + "']");
+	if (cat) {
+		$('<block type="console">' +
+			'</block>').appendTo(cat);
 	}
 };
