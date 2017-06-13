9b2dfade6b5281b1d39855a9c9c7f7d14761925f
CarbonMan <CarbonMan@users.noreply.github.com>
2017-06-13 21:53:23 +1000

    Create console_define.js

---
+++ console/console_define.js
@@ -0,0 +1,19 @@
+Blockly.Blocks['console'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Display")
+        .appendField(new Blockly.FieldDropdown([
+	    [ T$.i18n("verbose"),"verbose"], 
+	    [ T$.i18n("info"),"info"], 
+	    [ T$.i18n("warn"),"warning"], 
+	    [ T$.i18n("error"),"error"]]), "type");
+    this.appendValueInput("NAME")
+        .setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
