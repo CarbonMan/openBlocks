a23afca72b4d523ce232330a3c34bec1df92e565
CarbonMan <CarbonMan@users.noreply.github.com>
2017-06-21 23:24:01 +1000

    Update blocks_to_jqPlot.js

--- output/dataPlot/blocks_to_jqPlot.js
+++ output/dataPlot/blocks_to_jqPlot.js
@@ -1,4 +1,13 @@
-
+$(function(){
+  // Build the toolbox categories and load the blocks into them.
+  // The blocks have a custom function toolbox$ to make it easier to 
+  // centralize the code.
+  T$.on("load toolbox categories", function(ev){
+    var cat = ev.toolbox.find("category[name='" + T$.i18n('Display') + "']");
+    if (!cat.length) 
+        qryCat = $("<category name='" + T$.i18n('Display') + "'></category>").appendTo(ev.toolbox);  
+  });
+});
 /**
 * Chart toolbox entries
 */
