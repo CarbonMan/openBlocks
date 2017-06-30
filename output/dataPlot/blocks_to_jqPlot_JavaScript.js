fe19dc1422d4bd4bb9e462819cbcc757780deaab
CarbonMan <CarbonMan@users.noreply.github.com>
2017-06-30 17:01:35 +1000

    Update blocks_to_jqPlot_JavaScript.js

--- output/dataPlot/blocks_to_jqPlot_JavaScript.js
+++ output/dataPlot/blocks_to_jqPlot_JavaScript.js
@@ -1,22 +1,19 @@
 Blockly.JavaScript['line_chart'] = function (block) {
+    var dataSource = Blockly.JavaScript.valueToCode(block, 'dataSource', Blockly.JavaScript.ORDER_ATOMIC);
     var text_title = block.getFieldValue('title');
     var text_yaxis = block.getFieldValue('yAxis');
     var text_xaxis = block.getFieldValue('xAxis');
     var firstcolumnxaxisticks = block.getFieldValue('firstColumnXaxisTicks') == 'TRUE';
     var statements_labels = Blockly.JavaScript.statementToCode(block, 'labels');
     // Convert the data from the dataBlock to a data series 
-    var code = 'var cb = T$.codeBlocks;\n';
-    code += 'var dataSeries = cb.outputIntoSeries({' +
-        'context: this,' +
-        'firstColumnXaxisTicks:' + (firstcolumnxaxisticks ? "true" : "false") +
-        '});\n';
     // Run the chart against the data series
-    // The code is run in the codeBlocks.context
-    code += 'cb.plot.call(this, {' +
-        'uid: uid,' +
-        'tm: tm,' +
-        'dataBlock: this.__dataBlock,' +
-        'data: dataSeries' +
+    // The code is run in the CodeBlockTree context
+    var code = 'T$.codeBlocks.plot({' +
+        'context: me.context,' +
+        'uid: me.uid,' +
+        'threadId: me.threadId,' +
+        'data: ' + dataSource + ',' + 
+        'firstColumnXaxisTicks:' + (firstcolumnxaxisticks ? "true" : "false") +
         '});\n';
     return code;
 };
