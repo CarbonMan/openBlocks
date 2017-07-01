65a3ce1470f6d0bb95be12f6c8d063a66c59c824
CarbonMan <CarbonMan@users.noreply.github.com>
2017-07-01 14:45:57 +1000

    Update baseBlock_JavaScript.js

--- codeBlocks/baseBlock_JavaScript.js
+++ codeBlocks/baseBlock_JavaScript.js
@@ -41,5 +41,5 @@ Blockly.JavaScript['variables_set'] = function(block) {
       Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
   var varName = Blockly.JavaScript.variableDB_.getName(
       block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
-  return 'vars["' + varName + '"]=' + varName + ' = ' + argument0 + ';\n';
+  return 'vars["' + varName + '"]=' + argument0 + ';\n';
 };
