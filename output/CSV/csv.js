8e61dbfa66ac042880d1da03fa10e2747f3b43fd
CarbonMan <CarbonMan@users.noreply.github.com>
2017-07-02 08:36:02 +1000

    Create csv.js

---
+++ output/CSV/csv.js
@@ -0,0 +1,18 @@
+$(function(){
+  // Build the toolbox categories and load the blocks into them.
+  // The blocks have a custom function toolbox$ to make it easier to 
+  // centralize the code.
+  T$.on("load toolbox categories", function(ev){
+    var cat = ev.toolbox.find("category[name='" + T$.i18n('Display') + "']");
+    if (!cat.length) 
+        qryCat = $("<category name='" + T$.i18n('Display') + "'></category>").appendTo(ev.toolbox);  
+  });
+});
+/**
+* csv entries
+*/
+Blockly.Blocks['csv'].toolbox$ = function($toolbox){
+     var cat = $toolbox.find("category[name='" + T$.i18n('Display') + "']");
+     if (cat)
+         $('<block type="csv"></block>').appendTo(cat);
+};
