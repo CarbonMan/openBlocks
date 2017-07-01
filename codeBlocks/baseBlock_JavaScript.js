423435d7217cb41e21ca914e8ffcf866ca10e9c7
CarbonMan <CarbonMan@users.noreply.github.com>
2017-07-01 14:57:46 +1000

    Update baseBlock_JavaScript.js

--- codeBlocks/baseBlock_JavaScript.js
+++ codeBlocks/baseBlock_JavaScript.js
@@ -41,5 +41,7 @@ Blockly.JavaScript['variables_set'] = function(block) {
       Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
   var varName = Blockly.JavaScript.variableDB_.getName(
       block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
-  return 'vars["' + varName + '"]=' + argument0 + ';\n';
+  // Must set both the global and local copies of the variable. The "getter" above is not consistently invoked.
+  // See nlsql_js for an example.
+  return 'vars["' + varName + '"]=' + varName + '=' + argument0 + ';\n';
 };
