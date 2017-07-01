c1be14b0fe13e48bcaf7dbf201962a1f4d574eb3
CarbonMan <CarbonMan@users.noreply.github.com>
2017-07-01 13:08:04 +1000

    Update console_JavaScript.js

--- console/console_JavaScript.js
+++ console/console_JavaScript.js
@@ -1,7 +1,6 @@
 Blockly.JavaScript['console'] = function(block) {
   var type = block.getFieldValue('type');
   var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // vars[..] holds the values of the variables because they are public across platforms.
   code += "console." + type + "(" + value_name + ");\n";
   return code;
 };
