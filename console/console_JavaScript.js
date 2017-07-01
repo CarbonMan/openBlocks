763eb9b1dddfc31003029f33c97039b61a317100
CarbonMan <CarbonMan@users.noreply.github.com>
2017-07-01 13:02:06 +1000

    Update console_JavaScript.js

--- console/console_JavaScript.js
+++ console/console_JavaScript.js
@@ -2,6 +2,7 @@ Blockly.JavaScript['console'] = function(block) {
   var type = block.getFieldValue('type');
   var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = "console." + type + "(" + value_name + ");\n";
+  var code = value_name + " = vars['" + value_name + "'];\n";
+  code += "console." + type + "(" + value_name + ");\n";
   return code;
 };
