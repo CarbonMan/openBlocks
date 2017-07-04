45ec5db6d1ca138e03399ffe966aee9e78d5851b
CarbonMan <CarbonMan@users.noreply.github.com>
2017-07-04 17:19:35 +1000

    Update categories.js

--- output/categories.js
+++ output/categories.js
@@ -1,4 +1,4 @@
-T$.on("load toolbox categories", function(ev){
+T$_editor.on("load toolbox categories", function(ev){
   var cat = ev.toolbox.find("category[name='" + T$.i18n('Display') + "']");
   if (!cat.length)
     $("<category name='" + T$.i18n('Display') + "'></category>").appendTo(ev.toolbox);
